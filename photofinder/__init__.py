146767d0f59b55a100d0420eaf3d69c4eea417e0
Ivan Alsina <50876898+ialsina@users.noreply.github.com>
2025-12-28 19:34:28 +0100

    Add src

---
+++ photofinder/__init__.py
@@ -0,0 +1,15 @@
+from photofinder.protocols import (
+    calculate_hash,
+    check_disk_space,
+    cp,
+    mv,
+    rm,
+)
+
+__all__ = [
+    "calculate_hash",
+    "check_disk_space",
+    "cp",
+    "mv",
+    "rm",
+]
